fca679e24cb6a9897fe0fdaa9794df6831647ef0
in0412 <32053023+in0412@users.noreply.github.com>
2017-09-27 16:12:55 +0900

    프로젝트 최초등록

---
+++ HelloSpring/src/hello/MessageBeanEng.java
@@ -0,0 +1,12 @@
+package hello;
+
+public class MessageBeanEng implements MessageBean {
+
+	@Override
+	public void sayHello(String name) {
+		
+		// TODO Auto-generated method stub
+		System.out.println("Hello " + name);
+	}
+
+}
